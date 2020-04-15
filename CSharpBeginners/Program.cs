c4f9fcb75fcba1fdb73467cafd1e1e0875c3ce9b
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-16 00:48:49 +0500

    First Commit Section 4 Classes

--- CSharpBeginners/Program.cs
+++ CSharpBeginners/Program.cs
@@ -1,64 +1,25 @@
 ﻿using System;
 
-namespace Type_Conversion
+namespace Classes
 {
+    public class Person
+    {
+        public string fName;
+        public string lName;
+        public void Introduce()
+        {
+            Console.WriteLine("my name is " + fName + " " + lName);
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            /* Implicit Conversion
-            byte b = 1;
-            int i = b;
-            Console.WriteLine(i);
-            //.................................................................
-            
-            // Explicit Converesion
-            int c = 256;
-            byte d = (byte)c;
-            Console.WriteLine(d);
-            //.................................................................
-
-            // Non- Compatible conversitons.
-            var s = "1234";
-            var j = Convert.ToInt32(s);
-            Console.WriteLine(j); */
-
-            //.................................................................
-            // Exception Handling
-            /* var number = "1234";
-             var bbb= Convert.ToByte(number);
-             Console.WriteLine(bbb);
-             
-
-            //.................................................................
-            //Try catch
-            try
-            {
-                var number = "1234";
-                var bbb = Convert.ToByte(number);
-                Console.WriteLine(bbb);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("This conversion can not be done due to less space in byte!!!");
-
-            }*/
-
-            //.................................................................
-
-            //....................... OPERATORS ..............................
-            var z = 1;
-            var y = 2;
-            Console.WriteLine("a+b = " + z + y);
-            Console.WriteLine("a/b = " + z / y);
-            Console.WriteLine("a/b = " + (float)z / (float)y);
-            Console.WriteLine("a>b = " + (z > y));
-            Console.WriteLine("a==b = " + (z == y));
-            Console.WriteLine("a<b = " + (z < y));
-
-
-
-
+            Person person = new Person();  //var= Person
+            person.fName = "Huzaifa";
+            person.lName = "Shah";
+            person.Introduce();
         }
     }
 }
