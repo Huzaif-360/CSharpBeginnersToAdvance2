e4ecb542060c678fd1542e10632890ad6f49ac19
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-16 00:54:01 +0500

    Third Commit Section 4 Strings

--- CSharpBeginners/Program.cs
+++ CSharpBeginners/Program.cs
@@ -1,31 +1,23 @@
 ﻿using System;
 
-namespace Arrays
+namespace Strings
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] number = new int[3];
-            number[0] = 1;
-            Console.WriteLine(number[0]);
-            Console.WriteLine(number[1]);
-            Console.WriteLine(number[2]);
-            Console.WriteLine("...............................");
-            //.................................................................
-            var check = new bool[3];
-            check[0] = true;
-            Console.WriteLine(check[0]);
-            Console.WriteLine(check[1]);
-            Console.WriteLine(check[2]);
-            Console.WriteLine("...............................");
-            //.................................................................
-            var names = new string[3] { "Huzaifa", "Nouman", "Saad" };
-            //names[0] = "Huzaifa";
-            Console.WriteLine(names[0]);
-            Console.WriteLine(names[1]);
-            Console.WriteLine(names[2]);
-            Console.WriteLine("...............................");
+
+            string name = "Huzaifa";
+            var lastName = "Shah";
+            //.................... Method 1 ............................................
+            var fullName1 = name + " " + lastName;                                  
+            Console.WriteLine(fullName1);
+
+            //.................... Method 2 ............................................
+            var fullName2 = string.Format("{0} {1}  ", name, lastName);           
+            Console.WriteLine(fullName2);
+
+
         }
     }
 }
