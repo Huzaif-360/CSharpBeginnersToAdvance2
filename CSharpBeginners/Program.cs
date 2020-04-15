604ba61de98ed31a269842b0ec5c90b8cc4d08a5
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-16 00:13:49 +0500

    First commit Section 3 Implicit Conversion

--- CSharpBeginners/Program.cs
+++ CSharpBeginners/Program.cs
@@ -1,26 +1,15 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace Type_Conversion
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //clear console
-            Console.Clear();
-
-
-            Console.WriteLine("Hello World 2!");
-            // Beep sound
-            Console.Beep();
-         
-            var number = 1;
-            var count = 3;
-            var  fnum= 2.11f;
-
-            Console.WriteLine(number);
-            Console.WriteLine(fnum);
+            // Implicit Conversion
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
 
         }
     }
