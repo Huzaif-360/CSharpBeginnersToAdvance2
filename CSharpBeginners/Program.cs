b9525587b97ea30b1729a2237409028762757355
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-15 19:11:34 +0500

    initial commit

---
+++ CSharpBeginners/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpBeginners
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
