e968c6f702a85608331b7ecc699200622f895071
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-16 00:18:46 +0500

    Third Commit Section 3 Non-Compatible Concersion

--- CSharpBeginners/Program.cs
+++ CSharpBeginners/Program.cs
@@ -9,16 +9,22 @@ namespace Type_Conversion
             /* Implicit Conversion
             byte b = 1;
             int i = b;
-            Console.WriteLine(i);*/
+            Console.WriteLine(i);
             //.................................................................
             
             // Explicit Converesion
             int c = 256;
             byte d = (byte)c;
-            Console.WriteLine(d);
-           
+            Console.WriteLine(d);*/
+            //.................................................................
 
+            // Non- Compatible conversitons.
+            var s = "1234";
+            var j = Convert.ToInt32(s);
+            Console.WriteLine(j);
+            //.................................................................
 
+            
         }
     }
 }
