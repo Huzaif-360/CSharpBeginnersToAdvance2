485d345245e129c7db1466541f6491a18a43e245
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-16 15:47:19 +0500

    Completed Lecture 3 Built in Types

--- CSharpYouTubeTutorials/Program.cs
+++ CSharpYouTubeTutorials/Program.cs
@@ -7,30 +7,23 @@ namespace CSharpYouTubeTutorials
         static void Main()
         {
 
-            //UT Lecture 2
-            Console.WriteLine("Enter Your Name!");
-            string name= Console.ReadLine();
-
-            //................Method 1 Concatenation.....................
-            Console.WriteLine("My name is " + name);
-
-            //.............. Method 2  Place holder syntax .......................
-            Console.WriteLine("My name is {0}", name);
-
-            //...................................................................
-
-            //.....................CSharp is Case senstive ................
-            /* var NAME = "Huzaifa Shah";
-             Console.WriteLine(Name);
-             */
+            //UT Lecture 3
+            bool b = true;
+            // MAX and MIN values
+            int i;
+            Console.WriteLine("Min range {0}", int.MaxValue);
+            Console.WriteLine("Min range {0}", int.MinValue);
 
 
-            Console.WriteLine("Enter Your First Name!");
-            string fname = Console.ReadLine();
-            Console.WriteLine("Enter Your Last Name!");
-            string lname = Console.ReadLine();
-            Console.WriteLine("My name is {0} {1}", fname, lname);
+            //......................................
+            double d = 123.1234567;
+            Console.WriteLine(d);
+
+           
+
+
+
+
         }
-      
     }
 }
