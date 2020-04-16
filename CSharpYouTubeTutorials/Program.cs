6c48149519144e07e5bcbca896424eb309f3df50
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-16 15:23:04 +0500

    Completed Lecture 2 Read/Write From Console

--- CSharpYouTubeTutorials/Program.cs
+++ CSharpYouTubeTutorials/Program.cs
@@ -6,12 +6,31 @@ namespace CSharpYouTubeTutorials
     {
         static void Main()
         {
-            Console.WriteLine("Csharp YouTube Tutorials 1");
+
+            //UT Lecture 2
+            Console.WriteLine("Enter Your Name!");
+            string name= Console.ReadLine();
+
+            //................Method 1 Concatenation.....................
+            Console.WriteLine("My name is " + name);
+
+            //.............. Method 2  Place holder syntax .......................
+            Console.WriteLine("My name is {0}", name);
+
+            //...................................................................
+
+            //.....................CSharp is Case senstive ................
+            /* var NAME = "Huzaifa Shah";
+             Console.WriteLine(Name);
+             */
+
+
+            Console.WriteLine("Enter Your First Name!");
+            string fname = Console.ReadLine();
+            Console.WriteLine("Enter Your Last Name!");
+            string lname = Console.ReadLine();
+            Console.WriteLine("My name is {0} {1}", fname, lname);
         }
-       /* static void Main()
-        {
-            Console.WriteLine("Csharp YouTube Tutorials 2 ");
-            Main();
-        }*/
+      
     }
 }
