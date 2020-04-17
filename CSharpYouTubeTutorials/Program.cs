9bc0c8101576ca604ebd5fda4d383c8c96fd14f9
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-17 23:58:30 +0500

    Completed Lecture 8 Arrays

--- CSharpYouTubeTutorials/Program.cs
+++ CSharpYouTubeTutorials/Program.cs
@@ -7,64 +7,27 @@ namespace CSharpYouTubeTutorials
         static void Main()
         {
 
-            //UT Lecture 7
-            /*Type Conversion
-             * 1. Implicit (Simplest)
-             * 2. Explicit (Cascading)
-             * 3. Non-Compatible (uisng Convert.ToInt/ToString  function) 
-            */
-
-            //Type 1
-            int i = 100;
-            float f = i;
-            Console.WriteLine("float to int = {0}", f);
-            
-            //Type 2
-            float fl= 12.09f;
-            int j = (int)fl;
-            Console.WriteLine("int to float = {0}", j);
-
-            //Using Convert.ToInt32 function
-
-            float ff = 1234567890.123f;
-            int k = (int)ff;
-            Console.WriteLine("int to float = {0}", k);
-
-            /*
-            float ff = 123456789011223344.123f;
-            int k = Convert.ToInt32(ff);
-            Console.WriteLine("int to float = {0}", k);
-            */
-
-            //Type 3 
-            string s = "100";
-            int number = Convert.ToInt32(s);
-            Console.WriteLine("Convert.ToInt32 = {0}", number);
-
-            //Using Parse
-            string @str = "76584";
-            int number2 = int.Parse(@str);
-            Console.WriteLine("Convert.ToInt32 = {0}", number2);
-
-            //....................................
-            //............. Warning......TryParse ...........
-            string convertnumber = "76584";
-            int result = 0;
-
-
-            //int.TryParse(convertnumber, result);
-            bool check = int.TryParse(convertnumber, out result);
-           
-            if (check)
+            //UT Lecture 8
+            //............Arrays.............
+            int[] Arrays = new int[3];
+            Arrays[0] = 1;
+            Arrays[1] = 2;
+            Arrays[2] = 3;
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine("string to integer = {0}", result);
+                Console.WriteLine(Arrays[i]);
             }
-            else
+            //....................... insertion method 2...........
+            var A2 = new string[4] { "Ali", "Usama", "Shahid", "Huzaifa" };
+            for (int j=0; j<A2.Length; j++)
             {
-                Console.WriteLine("Your string can not be converted into integer. Pleases reonsider it");
+                Console.WriteLine("Element in Array at index {0} is {1}",j, A2[j]);
             }
-             
-            
+
+
+
+
+
 
 
 
