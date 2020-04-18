20649ac22e9efd49fa361f15821022c47d682e69
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-18 10:18:09 +0500

    Completed Lecture 9 Comments

--- CSharpYouTubeTutorials/Program.cs
+++ CSharpYouTubeTutorials/Program.cs
@@ -7,34 +7,49 @@ namespace CSharpYouTubeTutorials
         static void Main()
         {
 
-            //UT Lecture 8
-            //............Arrays.............
-            int[] Arrays = new int[3];
-            Arrays[0] = 1;
-            Arrays[1] = 2;
-            Arrays[2] = 3;
-            for (int i = 0; i < 3; i++)
-            {
-                Console.WriteLine(Arrays[i]);
-            }
-            //....................... insertion method 2...........
-            var A2 = new string[4] { "Ali", "Usama", "Shahid", "Huzaifa" };
-            for (int j=0; j<A2.Length; j++)
-            {
-                Console.WriteLine("Element in Array at index {0} is {1}",j, A2[j]);
-            }
+            //UT Lecture 9
+            //Comments
+
+            //................Type 1.(single)..............
 
 
+            //Console.WriteLine("Hellow World");
+            Console.WriteLine("Hellow World");
+
+            //..............Type 2 ...(Multiline)..................
+
+            /*
+             * Console.WriteLine("Hellow World");
+             * Console.WriteLine("Hellow World");
+             * Console.WriteLine("Hellow World");
+             * Console.WriteLine("Hellow World");
+             * Console.WriteLine("Hellow World");
+             */
 
 
+            //...............Type 3 .....(XML Documentation)...........
+            
 
+            //Console
+            //newclass.
 
 
 
 
 
         }
+        /// <summary>
+        /// This new sample class doing nothing 
+        /// </summary>
+        public class newclass
+        {
+
+        }
 
     }
 
 }
+ 
+ 
+ 
+ 
