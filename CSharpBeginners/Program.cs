8301b2183c443305e037c499c9a997aa290647eb
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-21 00:21:45 +0500

    Completed Section 6 Arrays and Lists

--- CSharpBeginners/Program.cs
+++ CSharpBeginners/Program.cs
@@ -1,46 +1,32 @@
 ﻿using System;
+//using System.Collections.Generic;
+
+
 namespace CSharpBeginners
 {
 
-    //Udemy 42;
+    
     class Program
     {
         
         static void Main(string[] args)
         {
             //Section 6 Arrays and Lists
-            //Lecture 
-
-            //Length
-            var Array1 = new int[3] { 1, 2, 3 };
-            Console.WriteLine("Length = "+ Array1.Length  );
-
-            //IndexOf
-            var index = Array.IndexOf(Array1, 3);
-            Console.WriteLine(index);
-
-            //Clear
-            Array.Clear(Array1, 0, 2);
-            //Check after Clear
-            foreach (var n in Array1)
+            //Lecture 54
+            //Lists
+            Console.WriteLine();
+            var List1 = (new int[5] { 1, 2, 3, 4, 5 }) ;
+            foreach(var n in List1)
             Console.WriteLine(n);
 
-            //Sorting 
-            Array.Sort(Array1);
-            //Check after Sorting
-            foreach (var n in Array1)
-            {
-                Console.WriteLine(n);
-            }
+            //IndexOf
+            Console.WriteLine(List1.IndexOf(2));
+
+            //Add Range
+            List1.Add(1);
+            List1.AddRange(new int[2] { 4, 5, 6 });
 
 
-            //Reverse of Array
-            Array.Reverse(Array1);
-            //Check after Reversing
-            foreach (var n in Array1)
-            {
-                Console.WriteLine(n);
-            }
         }
   
     }
