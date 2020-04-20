42aab8bf1e5956a0c067a57c6e33ed26182e041b
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-21 00:33:00 +0500

    Completed Lecture 58 Introduction to Date and Time

--- CSharpBeginners/Program.cs
+++ CSharpBeginners/Program.cs
@@ -11,21 +11,12 @@ namespace CSharpBeginners
         
         static void Main(string[] args)
         {
-            //Section 6 Arrays and Lists
-            //Lecture 54
-            //Lists
-            Console.WriteLine();
-            var List1 = (new int[5] { 1, 2, 3, 4, 5 }) ;
-            foreach(var n in List1)
-            Console.WriteLine(n);
+            /*Section 7 Date and Time
+            Lecture 58
+            Completed Lecture 58
+            Introduction*/
 
-            //IndexOf
-            Console.WriteLine(List1.IndexOf(2));
-
-            //Add Range
-            List1.Add(1);
-            List1.AddRange(new int[2] { 4, 5, 6 });
-
+            
 
         }
   
