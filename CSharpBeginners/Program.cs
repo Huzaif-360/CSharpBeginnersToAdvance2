62b512bee4521b123444ca886870b04ea78270aa
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-21 01:10:57 +0500

    First Commit Udemy Lecture 60 Time Span

--- CSharpBeginners/Program.cs
+++ CSharpBeginners/Program.cs
@@ -11,46 +11,23 @@ namespace CSharpBeginners
         
         static void Main(string[] args)
         {
-            /*Section 7 Date and Time
-            Lecture 59
-            Completed Lecture 58
-            Introduction*/
+            //Section 7 Date and Time
+            //Lecture 60
 
-            //Time Check
-            var dateTime = new DateTime(2020, 04, 21);
-            Console.WriteLine(dateTime);
+            //New Time Span
+            var timespam = new TimeSpan(3, 5, 7);
+            Console.WriteLine(timespam);
 
-            //Now
+            //............................
             var now = DateTime.Now;
             Console.WriteLine(now);
+            var start = DateTime.Now.AddMinutes(1);
+            var dif = now - start;
 
-            //Day
-            var day = DateTime.DaysInMonth(2020, 10);
-            Console.WriteLine("days in 10th month"+ day);
+            //diff is Time Span
+            Console.WriteLine("diff"+ dif);
 
-            Console.WriteLine("Year "+ now.Year);
-            Console.WriteLine("Month "+now.Month);
-            Console.WriteLine("Day "+now.Day);
-            Console.WriteLine("Minute "+now.Minute);
-            Console.WriteLine("Hour "+now.Hour);
-
-            //..........Tommorrow ...............
-            var tomorrow = now.AddDays(3);
-            Console.WriteLine(tomorrow);
-
-            //.............Yesterday ............
-            var yesterday = now.AddDays(-1);
-            Console.WriteLine(yesterday);
-
-            //C# Date Time formats
-            Console.WriteLine(now.ToLongDateString());
-            Console.WriteLine(now.ToShortDateString());
-            Console.WriteLine(now.ToLongTimeString());
-            Console.WriteLine(now.ToShortTimeString());
-            Console.WriteLine(now.ToString("dd-MM-yyyy"));
-            Console.WriteLine(now.ToString("yyyy-MM-dd"));
-
-
+            
 
         }
 
