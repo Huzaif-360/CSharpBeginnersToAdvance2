401e0b0df20052b519221de47053e2a895fab6cc
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-21 00:45:47 +0500

    First Commit Udemy Lecture 59 DateTime

--- CSharpBeginners/Program.cs
+++ CSharpBeginners/Program.cs
@@ -12,11 +12,28 @@ namespace CSharpBeginners
         static void Main(string[] args)
         {
             /*Section 7 Date and Time
-            Lecture 58
+            Lecture 59
             Completed Lecture 58
             Introduction*/
 
-            
+            //Time Check
+            var dateTime = new DateTime(2020, 04, 21);
+            Console.WriteLine(dateTime);
+
+            //Now
+            var now = DateTime.Now;
+            Console.WriteLine(now);
+
+            //Day
+            var day = DateTime.DaysInMonth(2020, 10);
+            Console.WriteLine("days in 10th month"+ day);
+
+            Console.WriteLine("Year "+ now.Year);
+            Console.WriteLine("Month "+now.Month);
+            Console.WriteLine("Day "+now.Day);
+            Console.WriteLine("Minute "+now.Minute);
+            Console.WriteLine("Hour "+now.Hour);
+
 
         }
   
