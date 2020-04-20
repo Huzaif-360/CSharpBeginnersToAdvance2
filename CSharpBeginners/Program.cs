a46e831d0173bce7cbf7ea3241618a90bf9d127d
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-21 01:20:16 +0500

    Completed Udemy Section 7 Time Span

--- CSharpBeginners/Program.cs
+++ CSharpBeginners/Program.cs
@@ -15,10 +15,12 @@ namespace CSharpBeginners
             //Lecture 60
 
             //New Time Span
-            var timespam = new TimeSpan(3, 5, 7);
-            Console.WriteLine(timespam);
 
-            //............................
+            var timespan = new TimeSpan(3, 5, 7);
+            Console.WriteLine(timespan);
+
+            /*
+             * /............................
             var now = DateTime.Now;
             Console.WriteLine(now);
             var start = DateTime.Now.AddMinutes(1);
@@ -26,8 +28,20 @@ namespace CSharpBeginners
 
             //diff is Time Span
             Console.WriteLine("diff"+ dif);
+            */
 
-            
+
+            //Add
+            Console.WriteLine("Add Example: " + timespan.Add(TimeSpan.FromMinutes(10)));
+
+            //del hours
+            Console.WriteLine("less hours: " + timespan.Subtract(TimeSpan.FromHours(6)));
+
+            //Write this time in string
+            Console.WriteLine("To String:- "+ timespan.ToString());
+            Console.WriteLine("To Pasre "+ TimeSpan.Parse("10:08:19:59"));
+            //Console.WriteLine("To Pasre " + TimeSpan.C("10:08:19:59"));
+
 
         }
 
