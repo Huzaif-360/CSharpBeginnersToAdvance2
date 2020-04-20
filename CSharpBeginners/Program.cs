9c9503f7082490623f91ee3da8fe5153bb7fa13a
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-21 01:44:52 +0500

    First Commit Udemy Lecture 64 Strings (Demo)

--- CSharpBeginners/Program.cs
+++ CSharpBeginners/Program.cs
@@ -11,19 +11,32 @@ namespace CSharpBeginners
         
         static void Main(string[] args)
         {
-            /*Section 8
-             * Working with text
-             
-            Strings
-	        Text
-	        Formating
-	        Searching
-	        Subtracting
-	        Replacing
-   	        Null Check
-	        Split
+            ///Section 8
+            //Working with text
+            //Lecture 64
 
-            */
+
+            //String to int
+            var num = "123";
+            var str = int.Parse(num);
+            Console.WriteLine(str);
+
+
+            //int to string
+            var n1 = 123;
+            var n2 = Convert.ToInt32(n1);
+            Console.WriteLine(n2);
+
+            //Trim Name   ...............Trim Extra Spaces.........
+            var myfullname = "Muhammad Huzaifa Shah     ";
+            myfullname = myfullname.Trim();
+            Console.WriteLine("My Trip Name = {0}", myfullname);
+
+            //...............
+            var index = myfullname.IndexOf(" ");
+
+
+
 
 
         }
