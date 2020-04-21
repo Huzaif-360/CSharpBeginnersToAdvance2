ca56a94443619fb896ae91f554743d0379f2d52d
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-22 00:22:18 +0500

    Completed Udemy Lecture 67 Summarising Text

--- CSharpBeginners/Program.cs
+++ CSharpBeginners/Program.cs
@@ -10,44 +10,50 @@ namespace CSharpBeginners
     
     class Program
     {
-        
+
         static void Main(string[] args)
         {
             ///Section 8
             //Working with text
             //Lecture 69
             var sentence = "very very very very very very very very very very very very very very very very much long sentence";
-            const int count = 20;
-            if (sentence.Length < count)
+            var summary = SummerizingText(sentence);
+            Console.WriteLine(summary);
+        }
+           
+        //Method Creation
+            static string SummerizingText(string text, int count =20)
             {
-                Console.WriteLine(sentence);
-            }
-            else
-            {
-                var words = sentence.Split(' ');
+                //const int count = 20;
+
+                if (text.Length < count)
+
+                    return text;
+                
+                var words = text.Split(' ');
                 var totalchar = 0;
 
 
                 //List for strings 
                 var summarywords = new List<string>();
 
-                foreach(var word in words)
+                foreach (var word in words)
                 {
-                    
+
                     totalchar = word.Length + 1;
                     summarywords.Add(word);
                     if (totalchar > count)
                         break;
                 }
                 //join
-                var summary= String.Join(" ", summarywords + "...");
-                Console.WriteLine(summary);
+                var summary = String.Join(" ", summarywords + "...");
+                return summary;
 
+                
             }
-
 
 
         }
 
     }
-}
+
