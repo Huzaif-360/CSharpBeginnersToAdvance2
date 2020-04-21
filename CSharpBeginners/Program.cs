13b322eb3bd2dcb25ed2487a919523346f47a8c5
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-22 00:52:00 +0500

    Completd Udemy Lecture 70 Summary

--- CSharpBeginners/Program.cs
+++ CSharpBeginners/Program.cs
@@ -15,20 +15,7 @@ namespace CSharpBeginners
             ///Section 8
             //Working with text
 
-            Console.Write("Enter a word: ");
-            // Note the ToLower() here. This is used to count for both A and a. 
-            var input = Console.ReadLine().ToLower();
-
-            var vowels = new List<char>(new char[] { 'a', 'e', 'o', 'u', 'i' });
-            var vowelsCount = 0;
-            foreach (var character in input)
-            {
-                if (vowels.Contains(character))
-                    vowelsCount++;
-            }
-
-            Console.WriteLine(vowelsCount);
-
+            //Completed Summary
 
 
 
