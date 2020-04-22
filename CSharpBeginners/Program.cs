9bf09c492c3fbe877f5430fe2f1a4f5053f7c38f
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-22 21:00:48 +0500

    Completed Udemy Lecture 72 System.IO Theory

--- CSharpBeginners/Program.cs
+++ CSharpBeginners/Program.cs
@@ -17,8 +17,46 @@ namespace CSharpBeginners
             //System.IO NameSpace
             
              Namespace is a class of classes
+             Fileinfo:
+                instance methods(access for all time)
+                only checked for once.
+             file:
+                static methods
+                only accessable by specific object.
              
-             
+             Create()
+             Copy()
+             Delete()
+             Exists()
+             GetAttributes()
+             Move()
+             Readline()
+
+
+            Directory: 
+                static methods
+
+            Directoryinfo: 
+                instance methods
+                
+            
+             CreateDirectory()
+             Delete()
+             Exists()
+             GetCurrentDirectory()
+             GetFiles()
+             Move()
+             GetLogicalDrives()
+
+
+            Path
+            GetDirectoryName()
+            GetFileName()
+            GetExtension()
+            GetTempPath()
+
+
+
              */
 
 
