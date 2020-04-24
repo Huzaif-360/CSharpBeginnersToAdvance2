50526f61981b7dc65d278a662332a39892b380d3
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-25 01:02:23 +0500

    Completed Uemdey Section 1 About Course C# Intermediate Level

---
+++ CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpIntermediateToAdvance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * C# Intermediate Course
+             * Completed Inreoduction
+             */
+            Console.WriteLine("Hello Huzaifa and Welcome to the new course!");
+        }
+    }
+}
