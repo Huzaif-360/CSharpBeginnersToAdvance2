87870ca505e169bcabc58996f28a31f5958cead9
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-25 20:28:11 +0500

    Completed Intermediate Lecture 15 Summary

--- CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
+++ CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
@@ -2,8 +2,8 @@
 //using System;
 //using System.Collections.Generic;
 //using System.Runtime.CompilerServices;
-using System;
-using System.Collections.Generic;
+//using System;
+//using System.Collections.Generic;
 
 namespace CSharpIntermediateToAdvance
 {
@@ -13,43 +13,14 @@ namespace CSharpIntermediateToAdvance
         static void Main(string[] args)
         {
 
-            /*
-        //Lecture 14
-        Indexers:
 
-        #using indexer
-        An indexer is a way to access elements in a class that represents a list of values.
-        
-            
-            var cookie = new HttpCookie //to see the name of user.
-        var name = cookie["name"]   (this is indexer)
-        var name = cookie.Getitem("name");  (without inderer we have to write a Getname method)
+            //Lecture 15
+            //Complted Summary (Review)
 
-        public string this[string key]
-        {
-        get {----}
-        set{----}
-        }
 
-            */
+
 
         }
-        public class Person
-        {
-             
-        private readonly Dictionary<string, string> _dictionary = new Dictionary<string, string>();
-            public DateTime Expiry { get; set; }
-
-            public string this[string key]
-            {
-                get => _dictionary[key];
-                set
-                {
-                    if (key != null) _dictionary[key] = value;
-                }
-            }
-        }
-
     }
 }
 
