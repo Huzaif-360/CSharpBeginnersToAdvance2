ad1deae3db3d62d1eef726d0ebecaca1a481df64
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-25 17:18:51 +0500

    Completed Intermediate Lecture 8 Construstors

--- CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
+++ CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
@@ -1,17 +1,60 @@
-﻿using System;
-
+﻿//using System;
+using System.Collections.Generic;
+//using System.Runtime.CompilerServices;
 namespace CSharpIntermediateToAdvance
 {
     class Program
     {
+        /* public void Myintroduction(string name)
+         {
+             Console.WriteLine("My name is" );
+         }*/
         static void Main(string[] args)
         {
 
             /*
              * C# Intermediate Course
-             * Completed Inreoduction
+             * 
+             * //Lecture 8
+            Construstor
+            Constructor overloading:
+                            (same name as class but with different paarameter).
+    
+                1. Create public class
+                    crete a seperate file customer.cs
+                2. different parameter construstors
+                3. construstor call wit attribute
+                4. public List<order> Orders;
              */
-            Console.WriteLine("Hello Huzaifa and Welcome to the new course!");
+
         }
+        public class Customer
+        {
+            public string Name;
+            public int Id;
+            public List<Order> Orders;
+
+            public Customer()
+            {
+                Orders = new List<Order>();
+            }
+
+            public Customer(int id)
+                : this()
+            {
+                this.Id = id;
+            }
+
+            public Customer(int id, string name)
+                : this(id)
+            {
+                this.Name = name;
+            }
+
+        }
+    }
+
+    internal class Order
+    {
     }
 }
