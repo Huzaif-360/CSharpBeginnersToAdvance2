84cb7fd8ba047fb75ccca9fe616b04565e1d6252
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-27 14:43:44 +0500

    Completed Intermediate Lecture 21 Summary

--- CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
+++ CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
@@ -12,40 +12,8 @@ namespace CSharpIntermediateToAdvance
 			//    System.Console.WriteLine("This section is exercise section and 1 example done and 2nd is completed logically");
 
 
-			/*Lecture 20
-			//Favour Composition over Inheritance
-
-			1. Problem with Inheritance
-			Advantages: 
-					Code- Resue/ AND EASY TO RESUE
-			DisAdvantages:
-					1. Easily absued by designers/developers
-					2. Large hierarchies
-					3. Fragility
-					4. Cause Tighlty Coupling
-			EXAMPLE: Living things in a house i.e. Human/ Animals both of them can eat/steep/walk but what if we add fish, can fish walk?? this is error.
-
-
-			2. Advantages of Composition(Association) over Inheritance
-				Inheritance relation can be converted to compoition. This is FLEXIBILITY.
-
-			EXAMPLE: Same as used in Inheritance:
-				Living things in a house i.e. Human/ Animals both of them can eat/steep/walk but what if we add fish, can fish walk?? this is error.
-			NO it is not ERROR here. we crear new classes. 
-			Different Classes:
-
-			Person
-			Dog
-			Fish
-			Duck 
-
-			Classes(Functions): Animal, Walkable, Swiimmable:
-			This give us fleixbel and lose coupling
-
-			SUMMARY:
-				2 TYPES OF CLASSES:
-					1. INHERITANCE (IS-A RELATIONSHIP)
-					2.  COMPOSITION (HAS- A RELATIONSHIP)
+			/*Lecture 21
+			/ Summary Part
            */
 		}
 
