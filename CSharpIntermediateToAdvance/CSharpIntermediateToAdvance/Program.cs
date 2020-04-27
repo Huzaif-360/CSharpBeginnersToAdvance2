e2d2bff74adeca17844ec0f333b6d60e64d85623
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-27 14:33:27 +0500

    Completed Intermediate Lecture 18 Inheritance

--- CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
+++ CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
@@ -1,4 +1,4 @@
-﻿//using System;
+﻿using System;
 //using System.Runtime.CompilerServices;
 //using System.Collections.Generic;
 
@@ -9,27 +9,57 @@ namespace CSharpIntermediateToAdvance
 
         static void Main(string[] args)
         {
-        //    System.Console.WriteLine("This section is exercise section and 1 example done and 2nd is completed logically");
-    
-        //Lecture 17
-        /*Class Coupling
-            A Coupling is a measure of how interconnected classes and subsystems are at the beginning of this course
-        
-            1. Depndency.
-            2. Tightly Couple Classes
-            3.  Loosely Couple Classes
+            //    System.Console.WriteLine("This section is exercise section and 1 example done and 2nd is completed logically");
 
-                 How to make  Loosely Couple Classes
-                    1. Encapsulation
-                    2. The relationsships between classes
-                    3. Interfaces
 
-                    Interfaces:
-                        Inheritance and Compositions
-                        Composiotion over Inheritances
-        */
-                }
+            /*Lecture 18:
+              Inheritance   
+                (IS a Relationship)
+                    ::: CAR is a Vehicle
+
+           What is Inheritance?
+                Inheritance is simply a kind of relationship between two classes that allows one to inherit code from the other is usually referred to as is a relationship.
+
+           Why we use it?
+                1. Code re-use
+                2. Polymorphic Uses
+
+           In UML:
+                Representing Object: (SUPER CLASS)
+                        Parent/ Base CLass
+                Child Class: (SUB CLASS)
+                        Derived Class 
+            */
+        }
+        //Class 1:
+        public class PresentationObject
+        {
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public void Copy()
+            {
+                Console.WriteLine("Object copied to clipboard.");
+            }
+
+            public void Duplicated()
+            {
+                Console.WriteLine("Object was duplicated.");
+            }
+        }
+        //Class 2:
+        public class Text : PresentationObject
+        {
+            public int FontSize { get; set; }
+            public string FontName { get; set; }
+
+            public void AddHyperlink(string url)
+            {
+                Console.WriteLine("Added a link" + url);
+            }
+        }
     }
+}
 
 
 
