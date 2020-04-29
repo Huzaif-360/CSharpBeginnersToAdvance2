8ba966318c4422e3f73bbb74bb99686427b2b4d2
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-29 16:15:14 +0500

    Completed Intermediate Lecture 33 What is an Interface?

--- CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
+++ CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
@@ -10,14 +10,26 @@ namespace CSharpIntermediateToAdvance
         static void Main(string[] args)
         {
 			/* 
-             * //Section 5
-            Polymorphism: Third  Pillar of OOP
+             * ///Section 6
+                Interfaces
+                Lecture 33
+                WHAT ARE INTERFACES:
+	            An interface is a language construct that is similar to class in terms of syntax but it's fundamentally
+                Starts with "I".
+                public interface ITaxcalculator
+                {
+                int Calculator();
+                }
+                    Don't have implementation
+                    no brakets
+                    no data types:
 
-            //Lecture 31
-            Completed Summary
-            Lecture 32
-            Exercise
-	*/
+                WHY WE NEED It?
+                To build loosely-coupled Applications.
+        
+                It has methods but no body.
+                They Improve Testibiliy and Extensibility
+*/
         }
     }
 }
