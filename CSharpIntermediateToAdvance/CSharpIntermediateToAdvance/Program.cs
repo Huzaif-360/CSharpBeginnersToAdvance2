e9c7158e8693daef124ce51fe493423de6e253d4
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-29 11:30:55 +0500

    Completed Intermediate Lecture 30 Sealed Classes and Members

--- CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
+++ CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿//using System;
 //using System.Runtime.CompilerServices;
-using System.Collections.Generic;
+//using System.Collections.Generic;
 
 namespace CSharpIntermediateToAdvance
 {
@@ -9,90 +9,30 @@ namespace CSharpIntermediateToAdvance
 
         static void Main(string[] args)
         {
-            /* 
+			/* 
              * //Section 5
             Polymorphism: Third  Pillar of OOP
 
-            
-            //Lecture 29
-            Abstract Classes and Members
-
-            1. Abstract Modifiers:
-	            Indicates that a class or a member is missing implementation
-            
-            public astract class shape
-            {
-            public abstract void Draw();
-            }
-            public class Circle: Shape
-            {
-            public override void Draw()
-            {
-            }   
-            }
-
-
-            2. Abstract Member:
-	        Rules:	
-	            1.Do not have any body,( implementation).
-		        public abstract void Draw();
+            //Lecture 30
+			Sealed Classes and Members
+					Oposite of Abstract Classes
+						Prevents from overriding OR
+						Prevents from derivation of classes	
 	
-	            2. If a member is declared as abstract, the containing class need to be declared as abstract too.
-	            public astract class shape
-	            {
-	            public abstract void Draw();
-	            }
-		
-	            3. In a derived class you must implement all abstract members in the base abstract class.
-	            public class Circle: Shape
-	            {
-	            public override void Draw()
-	            {
-	            }
-	            }
-	
-	            4. Abstract classes can not be instaniated
-	            var shape=new Shaoe(); //this won't compile
+					
+			public class Circle: Shape
+				{
+					public override void Draw()
+					{
+					Console.WriteLine("Draw a Circle");
+					}
+				}
 
-                3. Why to use Abstract?
-	            when you want to provide some common behavior while forcing other
-                developers to follow your design.
-                in our shaping example are declaring the draw method as abstract. We'll make sure that anytime a developer derives a class from the shape 
-                they will provide an implementation for the draw method. If we did not declare this method as abstract and instead provided an empty virtual method 
-                we would run into the risk of a developer deriving a class from the shape without overriding the draw method.
-
-
-                    */
-
-            //Creating a List of Shapes
-            var shapes = new List<Shape>();
-
-            //Creating a Circle
-            shapes.Add(new Circle());
-
-            //Creating a Rectangle
-            //shapes.Add(new Rectangle());
-
-            //Creating a Triangle
-            //shapes.Add(new Triangle());
-
-           // var canvas = new Canvas();
-            //anvas.DrawShapes(shapes);
-        }
-
-        //Parent Class
-        public abstract class Shape
-        {
-            public abstract void Draw();
-        }
-
-        //Derived Class 1
-        public class Circle : Shape
-        {
-            public override void Draw()
-            {
-                Console.WriteLine("Draw a Circle1.");
-            }
+				Sealed Classes are faster because they are of some run time optimizations.
+				This is least usable, this mess the inheritance hirarch
+				Some Classes are definded in .NET which are defind as sealed classe 
+				i.e. string class we can not inherit from it. but to use it we need extension methods.
+	*/
         }
     }
 }
