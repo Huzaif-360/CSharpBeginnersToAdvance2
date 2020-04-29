b4d35f0ec6114221620bcb9fb5c61a36d3dae69f
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-29 16:01:57 +0500

    Completed Intermediate Section 5  Polymorphism: Third  Pillar of OOP

--- CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
+++ CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
@@ -13,25 +13,10 @@ namespace CSharpIntermediateToAdvance
              * //Section 5
             Polymorphism: Third  Pillar of OOP
 
-            //Lecture 30
-			Sealed Classes and Members
-					Oposite of Abstract Classes
-						Prevents from overriding OR
-						Prevents from derivation of classes	
-	
-					
-			public class Circle: Shape
-				{
-					public override void Draw()
-					{
-					Console.WriteLine("Draw a Circle");
-					}
-				}
-
-				Sealed Classes are faster because they are of some run time optimizations.
-				This is least usable, this mess the inheritance hirarch
-				Some Classes are definded in .NET which are defind as sealed classe 
-				i.e. string class we can not inherit from it. but to use it we need extension methods.
+            //Lecture 31
+            Completed Summary
+            Lecture 32
+            Exercise
 	*/
         }
     }
