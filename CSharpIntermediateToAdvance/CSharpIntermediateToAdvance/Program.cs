c3f1ccdc80273c01d2b1b36bd51cc26897de606f
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-30 01:58:45 +0500

    Completed Intermediate Lecture 36 Interfaces are NOT Multiple Inheritance

--- CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
+++ CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
@@ -1,93 +1,26 @@
-﻿using System;
+﻿//using System;
 //using System.Runtime.CompilerServices;
 //using System.Collections.Generic;
 
-using System.IO;
+
 
 namespace CSharpIntermediateToAdvance
 {
     partial class Program
     {
-        public interface ILogger
-        {
-            void LogError(string message);
-            void LogInfo(string message);
-        }
+        /*Lecture 36
+        Interfaces are NOT Multiple Inheritance
 
-        static void Main(string[] args)
-        {
-            /* 
-             * ///Section 6
-                Interfaces
-                Lecture 35
-               Interface and Extensibility
-*/
-            //creating a new orderprocessor object
-        }
-        //........................................................
+        interfaces can not be implemented multiple.mean this is just delared mutiple.
+        Interface are not used for implementing multiple inheritance.
+        Multiple ineritance we implement it physically many time,
 
-        public class DbMigrator
-        {
-            private readonly ILogger _logger;
 
-            public DbMigrator(ILogger logger) => _logger = logger;
+        Inheritance: gives ploymorphic behaviour
 
-            public void Migrator()
-            {
-                object p = _logger.LogInfo("Migrating started at " + DateTime.Now);
-
-                //Details of migrating the database
-
-                _logger.LogInfo("Migrating finished at " + DateTime.Now);
+            */
             }
-        }
-
-
-        //........................................................
-        public class FileLogger : ILogger
-        {
-            private readonly string _path;
-
-            public FileLogger(string path)
-            {
-                _path = path;
             }
-            public void LogError(string message)
-            {
-                Log(message, "it is error ");
-            }
-
-            public void LogInfo(string message)
-            {
-                Log(message, "this is info message");
-            }
-
-            private void Log(string message, string messageType)
-            {
-                using (var streamWriter = new StreamWriter(_path, true))
-                {
-                    streamWriter.WriteLine(messageType + ": " + message);
-                }
-            }
-        }
-        //........................................................
-
-        public class ConsoleLogger : ILogger
-        {
-            public void LogError(string message)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(message);
-            }
-
-            public void LogInfo(string message)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(message);
-            }
-        }
-    }
-}
 
 
 
