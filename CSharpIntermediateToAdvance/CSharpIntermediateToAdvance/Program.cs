128fd0c869b7148b5d7f3773610041a3fe241817
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-01 20:26:42 +0500

    Completed Execiese 1 From Section 4

--- CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
+++ CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
@@ -1,15 +1,28 @@
-﻿
-//using System.Runtime.CompilerServices;
-//using System.Collections.Generic;
-//using System.Net.Mail;
-//using System.Text;
+﻿using System;
 
 namespace CSharpIntermediateToAdvance
 {
     partial class Program
     {
-        //Completed C# Intermediate Course
-         
+
+        static void Main(string[] args)
+        {
+            //Intermediate Section 4 Exercise
+            var stack = new Stack();
+            
+            //Push Status
+            stack.Push(19);
+            stack.Push(2);
+            
+
+
+            //Pop Status
+            Console.WriteLine("Count: {0}", stack.Size());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine("Count: {0}", stack.Size());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine("Count: {0}", stack.Size());
+        }
     }
 }
 
---
+++ CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Stack.cs
@@ -0,0 +1,82 @@
+﻿
+//using System;
+
+using System;
+using System.Collections;
+
+namespace CSharpIntermediateToAdvance
+{
+    public class Stack
+    { 
+    private readonly ArrayList _list = new ArrayList();
+
+    public void Push(object obj)
+    {
+        if (obj == null)
+        {
+            throw new InvalidOperationException("This is an Invalid Argument");
+        }
+        _list.Add(obj);
+    }
+
+    public object Pop()
+    {
+        var lastElementIndex = _list.Count - 1;
+        if (lastElementIndex < 0)
+        {
+            throw new InvalidOperationException("The Stack is already empty.");
+        }
+        var obj = _list[lastElementIndex];
+        _list.RemoveAt(lastElementIndex);
+        return obj;
+    }
+
+    public void Clear()
+    {
+            _list.Clear();
+    }
+
+    public int Size()
+    {
+        return _list.Count;
+    }
+}
+    public class Stack
+    {
+        private readonly ArrayList _list = new ArrayList();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("This is an Invalid Argument");
+            }
+            _list.Add(obj);
+        }
+
+        public object Pop()
+        {
+            var lastElementIndex = _list.Count - 1;
+            if (lastElementIndex < 0)
+            {
+                throw new InvalidOperationException("The Stack is already empty.");
+            }
+            var obj = _list[lastElementIndex];
+            _list.RemoveAt(lastElementIndex);
+            return obj;
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public int Size()
+        {
+            return _list.Count;
+        }
+    }
+}
+
+
+
