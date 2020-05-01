4d8ff2f53e2af40064c9e8cf03837664a94566a9
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-02 00:42:58 +0500

    Completed Intermediat Exercise 2

--- CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
+++ CSharpIntermediateToAdvance/CSharpIntermediateToAdvance/Program.cs
@@ -7,23 +7,109 @@ namespace CSharpIntermediateToAdvance
 
         static void Main(string[] args)
         {
-            //Intermediate Section 4 Exercise
-            var stack = new Stack();
-            
-            //Push Status
-            stack.Push(19);
-            stack.Push(2);
-            
+            //Section 5 Exercise
+            var instruction = "create a table Student";
+            var sqlConnection = new SqlConnection("Make a connection");
+            var oracleConnection = new OracleConnection("Make a connection");
+            var dbCommand = new DbCommand(instruction);
+            dbCommand.Execute();
+            dbCommand = new DbCommand(oracleConnection, instruction);
+            dbCommand.Execute();
+        }
 
 
-            //Pop Status
-            Console.WriteLine("Count: {0}", stack.Size());
-            Console.WriteLine(stack.Pop());
-            Console.WriteLine("Count: {0}", stack.Size());
-            Console.WriteLine(stack.Pop());
-            Console.WriteLine("Count: {0}", stack.Size());
+    }
+
+    //.....DB Commands
+    public class DbCommand
+    {
+        private readonly string _instruction;
+        private readonly DbConnection _dbConnection;
+        private SqlConnection sqlConnection;
+        private string instruction;
+
+        public DbCommand(DbConnection dbConnection, string instruction)
+        {
+            if (string.IsNullOrWhiteSpace(instruction))
+            {
+                throw new ArgumentNullException("These are Instructs:");
+            }
+            _instruction = instruction;
+            this._dbConnection = dbConnection ?? throw new ArgumentNullException("DbConnection");
+
+        }
+
+        public DbCommand(string instruction)
+        {
+            this.sqlConnection = sqlConnection;
+            this.instruction = instruction;
+        }
+
+        public void Execute()
+        {
+            _dbConnection.Open();
+            Console.WriteLine("Run: {0}", _instruction);
+            _dbConnection.Close();
         }
     }
+    //...........Class DB Connector
+    public abstract class DbConnection
+    {
+        private readonly string _connectionString;
+        public TimeSpan Timeout { get; set; }
+
+        public DbConnection(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentException("You must need to enter a valid string", "connectionString");
+            }
+
+            this._connectionString = connectionString;
+        }
+
+        public abstract void Open();
+        public abstract void Close();
+    }
+
+    //Derived Class DBConnector
+    public class OracleConnection : DbConnection
+    {
+        private bool _status;
+        public OracleConnection(string connectionString)
+            : base(connectionString)
+        {
+
+        }
+
+        public override void Open()
+        {
+            if (_status)
+            {
+                Console.WriteLine("Connection is already is open.");
+
+            }
+            else
+            {
+                Console.WriteLine("Opening Oracle Connection.");
+                _status = true;
+            }
+        }
+
+        public override void Close()
+        {
+            if (_status)
+            {
+                Console.WriteLine("Closing Oracle Connection.");
+                _status = false;
+            }
+            else
+            {
+                throw new InvalidOperationException("Before closing a connection you have to open it first.");
+            }
+        }
+    }
+    //.......End...............
 }
 
 
