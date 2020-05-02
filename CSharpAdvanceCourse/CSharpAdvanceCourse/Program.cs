dde55ffac8c9e1567a3770df5e3944d3f126723f
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-03 02:32:41 +0500

    First Commit Advance Lecture 9 Events

--- CSharpAdvanceCourse/CSharpAdvanceCourse/Program.cs
+++ CSharpAdvanceCourse/CSharpAdvanceCourse/Program.cs
@@ -4,93 +4,34 @@ using System.Collections.Generic;
 namespace CSharpAdvanceCourse
 {
 
-
-    //
-
-    /*Lecture 8 
-     * Lambda Expresion
-     * 
-     * An Anonymous method
-     *
-     * No access Modifier
-     * No name 
-     * No return statement
-     * 
-     * 
-     * WHY WE USE IT?
-     *      To make code short
-     *      to make code easy
-     *      for our convience
-    */
-
-
     class program
     {
         static void Main(string[] args)
         {
 
-            //Lambda Expression 
-            //agrs=> expression
-
-            //Square
-            //num => num * num;
-
-            /*If we don't pass arguments:
-             *  LE ........... ()=>...
-             *  
-             * ...............................................
-             * if we have 1 ar..gument:
-             * x=> .................
+            /*Lecture 9 
+             * Events
              * 
-             * * .................................................
-             * if we have many args:
-             * (x,y,z)=>.............
-             */
-
-            /*const int factor = 5;
-            Func<int, int> multiplier = n => n * factor;
-            Console.WriteLine(multiplier(10));
-            Func<int, int> square = num => num * num;
-            Console.WriteLine(square(5));
-            */
-            var books = new BookRepo().GetBooks();
-
-
-
-            //books.FindAll. this is Predicate Method.
-
-            //Normal Call
-            var cheapbook = books.FindAll(book => book.Price>10);
-
-            foreach(var book in cheapbook)
-            {
-                Console.WriteLine(book.title);
-
-            }
-            
-            
-            
-            
-            
-
-        }
-        /*Predicate Method
-        static bool IsCheaper(Book book)
-        {
-            return book.Price < 10;
-        }*/
-
-        public class BookRepo
-        {
-            public List<Book> GetBooks()
-            {
-                return new List<Book>
-                {
-                    new Book{ title = "Title 1", Price =7},
-                    new Book{ title = "Title 2", Price =11},
-                    new Book{ title = "Title 3", Price =71}
-                };
-            }
+             * WHAT IS EVENT:
+             *          A mechanism for communication betweeen Objects
+             *          
+             * WHY WE NEED IT:
+             *          Used in building Loosly Coupled Applications
+             *          Helps in extindind code
+             *          
+             * Tightly Coupled Classes are needed to be recompiled at every signle modification
+             * Loose Coupling reduces:
+             *          Compilation time
+             *          
+             *          
+             *      Event: Function that connect VideoEncode and MessageService is Event.
+             *      VideoService : this is parent and it does not know anything about existance of MessageService or MailService. 
+             *      It only know about only 1 method.
+             *      BUT:
+             *      how that method knows what to call??
+             *            This is done by using delegate.
+             *      
+            */ 
         }
     }
 }
