6d7a43315378cc0106bf00945f033a9aa27f09e7
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-05 21:58:16 +0500

    Second Commit Advance Lecture 12 Nullable Types

--- CSharpAdvanceCourse/CSharpAdvanceCourse/Program.cs
+++ CSharpAdvanceCourse/CSharpAdvanceCourse/Program.cs
@@ -3,6 +3,8 @@
 //using System.Threading;
 //using System.Linq;
 
+using System;
+
 namespace CSharpAdvanceCourse
 {
 
@@ -21,6 +23,28 @@ namespace CSharpAdvanceCourse
             DataBase :
             Customers.Birthday(datetime NULL)
             */
+
+
+            //DateTime date = null; Error bcoz datetime is not nullable
+            //we can make DateTime a nullable by using as following:
+
+            //Nullable<DateTime> date = null;    //No Error
+            ////Aslo we can write a nullable as:
+            //DateTime? date1 = null;
+            //int? num = null;
+
+
+
+            //Console.WriteLine(date.GetValueOrDefault());
+            //Console.WriteLine(date.HasValue);
+            //Console.WriteLine(date.Value);
+
+            DateTime? date = new DateTime(2020, 1, 1);
+            DateTime Date2 = date.GetValueOrDefault();
+            DateTime? date3 = Date2;
+            Console.WriteLine(date3.GetValueOrDefault());
+
+
 
 
         }
