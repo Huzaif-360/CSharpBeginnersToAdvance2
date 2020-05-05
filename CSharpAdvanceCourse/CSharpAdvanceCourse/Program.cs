903f6e983c046a046fb13d8bdaed2050faa1842f
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-05 21:04:11 +0500

    First Commit Advance Lecture 12 Nullable Types

--- CSharpAdvanceCourse/CSharpAdvanceCourse/Program.cs
+++ CSharpAdvanceCourse/CSharpAdvanceCourse/Program.cs
@@ -1,7 +1,7 @@
 ﻿//using System;
 //using System.Collections.Generic;
 //using System.Threading;
-using System.Linq;
+//using System.Linq;
 
 namespace CSharpAdvanceCourse
 {
@@ -11,35 +11,20 @@ namespace CSharpAdvanceCourse
         static void Main(string[] args)
         {
             /*
-             * Lecture 11
-             * LINQ
-             *  Language Integrated Query
-             *  LINQ gives the capability to query objects
-             *  
-             *  
-             *  
-             *  You can query:
-             *  Object in memory, eg collections (LINQ to Objects)
-             *  Databases (LINQ to Entities)
-             *  XML (LINQ to XML)
-             *  ADO.NET Data Sets (LINQ to Data Sets)
-             */
-
-            //LINQ Helps in reducing the lines of code.
-
-            //LINQ usage
-            var cheapbook = books.Where(b => b.Price < 10);
-            /*
-             * LINQ Methods:
-             * Where(............Use Lambda Expression..........)
-             * Orderby(............Use Lambda Expression..........)
-             * Single.(...........)  .........Single only works where the object must exists.
-             * if we are not sure that we must use SigleorDefault()
-             */
+             * Lecture 12
+             * Nullable Types
+             * 
+             * We Know:
+             * A value type can never have NULL
+             * i.e. Bool hasAssess= true; or false 
+            bool hasAssess = true; //or false
+            DataBase :
+            Customers.Birthday(datetime NULL)
+            */
 
 
         }
     }
-    //public class 
+    
 
 }
